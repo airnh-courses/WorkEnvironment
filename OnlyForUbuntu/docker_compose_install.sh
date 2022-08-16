19b4512b42022f07c712224ea0d7adbe7131ca9d
el <hylee101001@gmail.coim>
2022-08-15 21:56:51 -0700

    asdf

--- OnlyForUbuntu/docker_compose_install.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v3.1.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
-docker compose version
